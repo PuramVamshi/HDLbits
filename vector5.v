58a5f2e8021acabc96e70a1cc7d0f3ee80c99f66
Vamshi Puram <vamshipuram123@gmail.com>
2024-09-24 19:43:35 +0530

    Create vector5.v
    Given five 1-bit signals (a, b, c, d, and e), compute all 25 pairwise one-bit comparisons in the 25-bit output vector. The output should be 1 if the two bits being compared are equal.
    
    out[24] = ~a ^ a;   // a == a, so out[24] is always 1.
    out[23] = ~a ^ b;
    out[22] = ~a ^ c;
    ...
    out[ 1] = ~e ^ d;
    out[ 0] = ~e ^ e;

---
+++ vector5.v
@@ -0,0 +1,11 @@
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+    
+    assign out = ~ {a,a,a,a,a,b,b,b,b,b,c,c,c,c,c,d,d,d,d,d,e,e,e,e,e}^{a,b,c,d,e,a,b,c,d,e,a,b,c,d,e,a,b,c,d,e,a,b,c,d,e};
+
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs.
+    // assign out = ~{ ... } ^ { ... };
+
+endmodule
